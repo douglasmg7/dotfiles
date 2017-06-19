65e2d3928336450598dae239a001c770312b67f8
douglasmg7 <douglasmg7@gmail.com>
2017-06-19 10:59:10 -0300

    include tag for sublime

--- dwm/config.h
+++ dwm/config.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Atom",     NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Sublime",  NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
