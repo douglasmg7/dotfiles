85d6951ce9bbe3ceb57773702f3d70a22aadb12d
douglasmg7 <douglasmg7@gmail.com>
2018-04-13 14:10:09 -0300

    change tags and start up

--- dwm/config.h
+++ dwm/config.h
@@ -28,9 +28,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Atom",     NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Sublime",  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "st",       NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Sublime",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "chromium",  NULL,      NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
