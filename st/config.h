654c997443da7f1c7697adf74553484b1dc5e837
douglasmg7 <douglasmg7@gmail.com>
2019-06-11 15:49:05 -0300

    dwm menu

--- st/config.h
+++ st/config.h
@@ -136,8 +136,10 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+// static unsigned int cols = 80;
+static unsigned int cols = 60;
+// static unsigned int rows = 24;
+static unsigned int rows = 12;
 
 /*
  * Default colour and shape of the mouse cursor
