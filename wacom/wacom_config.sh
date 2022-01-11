902cecc4c6229a7613b64ffbf0c82bd84abc7af5
douglasmg7 <douglasmg7@gmail.com>
2022-01-11 12:33:21 -0300

    wacom config

---
+++ wacom/wacom_config.sh
@@ -0,0 +1,3 @@
+xsetwacom set  17 area 0 1280 15200 9500
+xsetwacom set 17 maptooutput "HEAD-0"
+xsetwacom set 17 rotate half
---
+++ wacom/wacom_config_ultra_wide.sh
@@ -0,0 +1,3 @@
+xsetwacom set "Wacom One by Wacom S Pen stylus" area 0 3250 15200 9500
+xsetwacom set "Wacom One by Wacom S Pen stylus" maptooutput "HEAD-1"
+xsetwacom set "Wacom One by Wacom S Pen stylus"  rotate half
